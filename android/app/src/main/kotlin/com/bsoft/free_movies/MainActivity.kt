0f754e576c04d8722b91867c10802e2b1d91080d
apocal <beqa91@gmail.com>
2020-11-11 15:51:17 +0400

    Initial commit

---
+++ android/app/src/main/kotlin/com/bsoft/free_movies/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.bsoft.free_movies
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constants/Constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const Color kBackgroundColor = Color(0XFF101010);
+const Color kLigthBackgroundColor = Color(0XFF151515);
+const Color kTitleColor = Color(0XFF656565);
+const Color kYelowTitleColor = Color(0XFFdeb845);
+const Color kHeaderColor = Color(0XFFa8a8a8);
+const Color kHeaderYelowColor = Color(0XFFea8d1f);
